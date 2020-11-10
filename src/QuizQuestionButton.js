4e4ae72c28be8e42154fdc6e9c124e21f74ac8f8
Anthony Nguyen <anthony.nguyen200@gmail.com>
2020-11-09 21:41:30 -0800

    Completed module

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,9 +4,9 @@ class QuizQuestionButton extends Component {
   render(){
     return(
       <li>
-        <button>
-        {this.props.button_text}
-        </button>
+      <button>
+      {this.props.button_text}
+      </button>
       </li>
     )
   }
